c75ccff93fb4d38cccd537325f918a4243727da5
xgfone <xgfone@126.com>
2024-12-06 14:44:57 +0800

    rename package name from nets to netx

--- netx/net.go
+++ netx/net.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package nets provides some convenient net functions.
-package nets
+// Package netx provides some convenient net functions.
+package netx
 
 import (
 	"errors"
