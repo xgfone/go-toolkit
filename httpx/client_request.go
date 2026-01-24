35eae039e3075957793a3f5782a4117707fe0d63
xgfone <xgfone@126.com>
2026-01-24 23:06:39 +0800

    httpx: update the doc of Get and Post

--- httpx/client_request.go
+++ httpx/client_request.go
@@ -27,16 +27,31 @@ import (
 
 // Get sends a GET request to the specified URL and decodes the response body
 // into the provided response object.
-func Get(ctx context.Context, url string, resp any) (err error) {
-	return request(ctx, http.MethodGet, url, resp, nil)
+//
+// The respbody parameter supports the following types:
+//   - nil: response body is ignored, only HTTP status code 200 is checked
+//   - func(*http.Response) error: custom response handler function
+//   - any other type: response body is automatically decoded as JSON into the variable
+func Get(ctx context.Context, url string, respbody any) (err error) {
+	return request(ctx, http.MethodGet, url, respbody, nil)
 }
 
 // Post sends a POST request to the specified URL with the provided request body
 // and decodes the response body into the provided response object.
 //
-// If the request body is not nil, it will set the Content-Type header to "application/json".
-func Post(ctx context.Context, url string, resp any, req any) (err error) {
-	return request(ctx, http.MethodPost, url, resp, req)
+// The respbody parameter supports the following types:
+//   - nil: response body is ignored, only HTTP status code 200 is checked
+//   - func(*http.Response) error: custom response handler function
+//   - any other type: response body is automatically decoded as JSON into the variable
+//
+// The reqbody parameter supports the following types:
+//   - nil: no request body will be sent
+//   - io.Reader: used directly as the request body
+//   - any other type: automatically encoded as JSON
+//
+// If reqbody is not nil, it will set the Content-Type header to "application/json".
+func Post(ctx context.Context, url string, respbody any, reqbody any) (err error) {
+	return request(ctx, http.MethodPost, url, respbody, reqbody)
 }
 
 func request(ctx context.Context, method, url string, resp, req any) (err error) {
