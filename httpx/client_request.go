cbd3d8625476d58cf743da911143b6ff99b19a55
xgfone <xgfone@126.com>
2026-03-07 22:10:44 +0800

    httpx: update the function doc

--- httpx/client_request.go
+++ httpx/client_request.go
@@ -108,12 +108,12 @@ func Post(ctx context.Context, url string, respbody any, reqbody any) (err error
 // Request sends an HTTP request to the specified URL with the provided request body
 // and decodes the response body into the provided response object.
 //
-// The respbody parameter supports the following types:
+// The resp parameter supports the following types:
 //   - nil: response body is ignored, only HTTP status code 200 is checked
 //   - func(*http.Response) error: custom response handler function
 //   - any other type: response body is automatically decoded as JSON into the variable
 //
-// The reqbody parameter supports the following types:
+// The req parameter supports the following types:
 //   - nil: no request body will be sent
 //   - io.Reader: used directly as the request body
 //   - func(ctx context.Context, method, url string) (req *http.Request, err error)
