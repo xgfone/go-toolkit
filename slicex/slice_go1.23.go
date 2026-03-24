619bd691410e38f02c92957563c4f53f57b20fe2
xgfone <xgfone@126.com>
2026-03-24 22:35:28 +0800

    slices: add ValuesFunc function

---
+++ slicex/slice_go1.23.go
@@ -0,0 +1,30 @@
+// Copyright 2026 xgfone
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build go1.23
+
+package slicex
+
+import "iter"
+
+// ValuesFunc returns an iterator that yields the slice elements in order.
+func ValuesFunc[Slice ~[]E, E, V any](s Slice, f func(E) V) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for _, v := range s {
+			if !yield(f(v)) {
+				return
+			}
+		}
+	}
+}
