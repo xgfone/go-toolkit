26732247c915bb6d154022d0dc1ad4e322ba198c
xgfone <xgfone@126.com>
2026-08-17 15:35:03 +0800

    errorx: rename the file

--- errorx/sensitive.go
+++ errorx/sensitive.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package errorx provides some extra errors.
 package errorx
 
 import "strings"
